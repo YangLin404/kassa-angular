8c1cae90bfff4062f266757d32228e3bffb71ed7
Yang Lin <yang.lin0487@gmail.com>
2017-10-15 15:21:28 +0200

    improve the sort algoritme of ticket items

--- src/app/components/ticket-component/ticket-item-sort.pipe.ts
+++ src/app/components/ticket-component/ticket-item-sort.pipe.ts
@@ -1,19 +1,38 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {TicketItem} from '../item-search-component/ticket-item';
+import {RestoItem} from '../item-search-component/resto-item';
 
 @Pipe({
   name: 'itemSort'
 })
 export class TicketItemSortPipe implements PipeTransform {
-  transform(array: any[], field: string): any[] {
-    array.sort((a: any, b: any) => {
-      if (a[field] < b[field]) {
+  transform(array: TicketItem[]): TicketItem[] {
+    array.sort((a: TicketItem, b: TicketItem) => {
+      const aOrder = this.getOrderId(a.item);
+      const bOrder = this.getOrderId(b.item);
+      if (aOrder < bOrder) {
         return -1;
-      } else if (a[field] > b[field]) {
+      } else if (aOrder > bOrder) {
         return 1;
       } else {
         return 0;
       }
     });
     return array;
+  }
+
+  private getOrderId(item: RestoItem): number {
+    switch (item.itemType) {
+      case 'Drink': { return 1; }
+      case 'Soup': { return 2; }
+      case 'Entree': { return 3; }
+      case 'MainDishe': { return 4; }
+      case 'Menu': {return 4; }
+      case 'Nasi': { return 5; }
+      case 'Bami': { return 6; }
+      case 'Supplement': {return 7; }
+      case 'Dessert': {return 8; }
+      default: {return 0; }
+    }
   }
 }
