ec3526fb4a5870a560a85ffd544cd93f63a8b386
yanglin <yang.lin0487@gmail.com>
2017-10-29 12:56:25 +0100

    implements config page - add reload data method

--- src/app/config-component/config.service.ts
+++ src/app/config-component/config.service.ts
@@ -1,11 +1,28 @@
 
 import {Injectable} from '@angular/core';
+import {NGXLogger} from 'ngx-logger';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class ConfigService {
   private baseUrl = 'http://redirectme.ddns.net:7777/api/';
 
+  constructor(private http: Http, private logger: NGXLogger) {}
+
   getRestAPIBaseUrl(): string {
     return this.baseUrl;
+  }
+
+  reloadData(): Promise<boolean> {
+    const url = this.baseUrl + 'config/reload';
+    return this.http.post(url, '', '')
+      .toPromise()
+      .then(response => response.json() as boolean)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
